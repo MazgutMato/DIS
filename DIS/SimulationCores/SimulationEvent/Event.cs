3c9523d5572332e4ed6ac5a0acff4da62548564f
Matej Mazgut <matej.mazgut@gmail.com>
2023-03-21 09:14:21 +0100

    EventCore and Event

---
+++ DIS/SimulationCores/SimulationEvent/Event.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DIS.SimulationCores.SimulationEvent
+{
+    public abstract class Event
+    {
+        public double _eventTime { get; set; }
+        public EventCore _myCore { get; set; }
+        public Event(double eventTime, EventCore core) { 
+            _eventTime = eventTime;
+            _myCore = core;
+        }
+        public abstract void Execute();
+    }
+}
---
+++ DIS/SimulationCores/SimulationEvent/EventCore.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DIS.SimulationCores.SimulationEvent
+{
+    public abstract class EventCore : SimulationCore
+    {
+        private PriorityQueue<Event,double> _priorityQueue;
+        public double _actualTime { get; set; }
+
+        public EventCore(int repCount) : base(repCount)
+        {
+            this._priorityQueue = new PriorityQueue<Event,double>();
+            this._actualTime = 0;
+        }
+
+        public void Simulate(double maxTime)
+        {
+            while (_actualTime < maxTime && _priorityQueue.Count > 0)
+            {
+                var actualEvent = _priorityQueue.Dequeue();
+                _actualTime = actualEvent._eventTime;
+
+                if (_actualTime < maxTime)
+                {
+                    actualEvent.Execute();
+                }
+            }
+        }
+
+        public bool AddEvent(Event addedEvent){
+            if(addedEvent != null && addedEvent._eventTime > 0)
+            {
+                _priorityQueue.Enqueue(addedEvent, addedEvent._eventTime); 
+                return true;
+            }
+            return false;
+        }
+    }
+}
